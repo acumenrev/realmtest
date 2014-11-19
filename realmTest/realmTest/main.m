1166c03967da94644d1345e86019884cb264b8ed
Tri Vo <acumen.vn@gmail.com>
2014-11-19 11:49:52 +0700

    - Init Push

---
+++ realmTest/realmTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  realmTest
+//
+//  Created by Tri Vo on 11/19/14.
+//  Copyright (c) 2014 Tri Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
